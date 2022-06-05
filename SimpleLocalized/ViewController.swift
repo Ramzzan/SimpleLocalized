376fd805d05f11375de5c7afe5435e332afe0a68
Waleed Ahmed <waleed.wit@gmail.com>
2022-06-05 09:38:36 +0300

    add message label

--- SimpleLocalized/ViewController.swift
+++ SimpleLocalized/ViewController.swift
@@ -9,11 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        
     }
 
-
+    
 }
 
